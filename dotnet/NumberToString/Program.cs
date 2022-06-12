84915332584e8ff01d1a8d0d3b869f33b68d12dc
alabliuk <andrelabliuk@gmail.com>
2022-06-12 17:27:55 -0300

    NumberToString

---
+++ dotnet/NumberToString/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Kata
+{
+    //--> https://www.codewars.com/kata/5265326f5fda8eb1160004c8/
+
+    public static void Main()
+    {
+        NumberToString(-1);
+    }
+
+    public static string NumberToString(int num)
+    {
+        return num.ToString();
+    }
+}
