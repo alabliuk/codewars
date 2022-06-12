b12e2a1c6539890b825dad8970c23bf2e6536035
alabliuk <andrelabliuk@gmail.com>
2022-06-12 16:42:42 -0300

    Usdcny

---
+++ dotnet/UsdCny/Program.cs
@@ -0,0 +1,14 @@
+﻿public static class Kata
+{
+    public static void Main()
+    {
+        Console.WriteLine(Usdcny(15));
+        Console.WriteLine(Usdcny(465));
+    }
+
+    public static string Usdcny(int usd)
+    {
+        float calc = usd * 675;
+        return string.Format("{0:#.00}", calc / 100) + " Chinese Yuan";
+    }
+}
