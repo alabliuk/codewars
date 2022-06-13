589db5a7b798ef6eee9cdc70ef79e7bf0006437f
alabliuk <andrelabliuk@gmail.com>
2022-06-13 00:51:31 -0300

    ReverseSeq

---
+++ dotnet/ReverseSeq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class Kata
+{
+    //--> https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+    public static void Main()
+    {
+        ReverseSeq(5);
+    }
+
+    public static int[] ReverseSeq(int n)
+    {
+        var seq = new int[n];
+        int aux = n;
+
+        for(int i = 0; i < n; i++)
+        {
+            seq[i] = aux;
+            aux--;
+        }
+
+        return seq;
+    }
+}
