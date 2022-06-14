e86e1bb4d28473309ac96e7bace38e11c64a257e
alabliuk <andrelabliuk@gmail.com>
2022-06-14 01:33:13 -0300

    ConvertNumberToReversedArrayOfDigits

---
+++ dotnet/ConvertNumberToReversedArrayOfDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    //--> https://www.codewars.com/kata/5583090cbe83f4fd8c000051/
+
+    public class Digitizer
+    {
+        public static void Main()
+        {
+            Digitize(0);
+        }
+
+        public static long[] Digitize(long n)
+        {
+            char[] intArray = n.ToString().ToCharArray();
+            Array.Reverse(intArray);
+            var longArray = new long[intArray.Length];
+
+            for(int i = 0; i < intArray.Length; i++)
+            {
+                longArray[i] = (long)char.GetNumericValue(intArray[i]);
+            }
+
+            return longArray;
+        }
+    }
+}
