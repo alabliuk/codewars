5c3228f7f4ecc280695142cd440ae99947f5c96a
alabliuk <andrelabliuk@gmail.com>
2022-06-13 23:55:08 -0300

    PowerOfTwo

---
+++ dotnet/PowersOfTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+public class Kata
+{
+    //--> https://www.codewars.com/kata/57a083a57cb1f31db7000028
+
+    public static void Main()
+    {
+        PowersOfTwo(0);
+    }
+
+    public static BigInteger[] PowersOfTwo(int n)
+    {
+        var array = new BigInteger[n + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            array[i] = (BigInteger)Math.Pow(2, i);
+        }
+
+        return array;
+    }
+}
