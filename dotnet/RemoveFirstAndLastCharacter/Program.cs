60752622a00f3883f7138c911178b5b503c4b287
alabliuk <andrelabliuk@gmail.com>
2022-06-14 01:48:59 -0300

    RemoveFirstAndLastCharacter

---
+++ dotnet/RemoveFirstAndLastCharacter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Kata
+{
+    public static void Main()
+    {
+        Remove_char("");
+    }
+
+    public static string Remove_char(string s)
+    {
+        if (s.Length < 2)
+            return string.Empty;
+
+        return s.Remove(s.Length - 1).Substring(1);
+    }
+}
