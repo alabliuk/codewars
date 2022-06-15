e298acdd9078c35554a702be7ceae14a8833a58e
alabliuk <andrelabliuk@gmail.com>
2022-06-15 01:42:15 -0300

    splitstring - pendente

---
+++ dotnet/_SplitStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿public class SplitString
+{
+    //--> https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
+
+    public static void Main()
+    {
+        Solution("abcdef");
+    }
+
+    public static string[] Solution(string str)
+    {
+        var teste = str.Split();
+
+        string[] x = new string[1];
+        return x;
+    }
+}
