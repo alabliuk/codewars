dce2be1dc79bbed3efb3eca055b7a9e1c242ca76
alabliuk <andrelabliuk@gmail.com>
2022-06-17 04:20:36 -0300

    DuplicateCount

---
+++ dotnet/CountingDuplicates/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Kata
+{
+    public static void Main()
+    {
+        DuplicateCount("aabBcde");
+    }
+
+    public static int DuplicateCount(string str)
+    {
+        var duplicates = str.ToLower().GroupBy(x => x)
+            .SelectMany(y => y.Skip(1))
+            .Distinct()
+            .ToList();
+
+        return duplicates.Count();
+    }
+}
