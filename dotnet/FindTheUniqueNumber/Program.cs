4155dab5f4c14c7a910ab9570c8b82400b5ed28f
alabliuk <andrelabliuk@gmail.com>
2022-06-17 03:51:22 -0300

    GetUnique

---
+++ dotnet/FindTheUniqueNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Kata
+{
+    public static void Main()
+    {
+        GetUnique(new[] { 11, 11, 14, 11, 11 });
+    }
+
+    public static int GetUnique(IEnumerable<int> numbers)
+    {
+        var duplicates = numbers.GroupBy(x => x)
+            .SelectMany(y => y.Skip(1))
+            .Distinct()
+            .ToList();
+
+        var newNumbers = numbers.ToList();
+
+        foreach (var z in duplicates)
+        {
+            newNumbers.RemoveAll(x => x == z);
+        }
+
+        return newNumbers.First();
+    }
+}
