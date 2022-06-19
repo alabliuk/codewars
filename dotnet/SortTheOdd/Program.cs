5d5e2dc3ec217fb737f2c23d250054df72c1efb7
alabliuk <andrelabliuk@gmail.com>
2022-06-19 17:26:41 -0300

    SortTheOdd

---
+++ dotnet/SortTheOdd/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Kata
+{
+    //--> https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/
+
+    public static void Main()
+    {
+        SortArray(new int[] { 5, 3, 2, 8, 1, 4 }); // 1, 3, 2, 8, 5, 4
+    }
+
+    public static int[] SortArray(int[] array)
+    {
+        int[] newArray = array;
+        var x = array.OrderBy(x => x).Where(x => x % 2 == 1).ToList();
+        int aux = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (newArray[i] % 2 == 1)
+            {
+                newArray[i] = x[aux];
+                aux++;
+            }                
+        }
+
+        return array;
+    }
+}
