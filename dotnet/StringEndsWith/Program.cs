1769851ae3ab3d9efe6a81b6da4e4b1bf83f44f1
alabliuk <andrelabliuk@gmail.com>
2022-06-19 16:31:59 -0300

    StringEndsWith

---
+++ dotnet/StringEndsWith/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Kata
+{
+    public static void Main()
+    {
+        Solution("samurai", "ai"); //true 
+        Solution("!@#$%^&*() :-)", ":-)"); //true
+        Solution("abc\n", "abc"); //false
+    }
+
+    public static bool Solution(string str, string ending)
+    {
+        return str.EndsWith(ending);
+    }
+}
