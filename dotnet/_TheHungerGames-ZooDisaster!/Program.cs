5e9ab59f388948df8a3b3be95d69b728bfdbf736
alabliuk <andrelabliuk@gmail.com>
2022-06-20 23:14:54 -0300

    WhoEatsWho

---
+++ dotnet/_TheHungerGames-ZooDisaster!/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Dinglemouse
+{
+    //--> https://www.codewars.com/kata/5902bc7aba39542b4a00003d
+
+    public static void Main()
+    {
+        WhoEatsWho("fox, bug, chicken, grass, sheep");
+    }
+
+    public static string[] WhoEatsWho(string zoo)
+    {
+        var arrayZoo = zoo.Replace(" ", "").Split(",");
+
+
+        return new string[1];
+    }
+}
