9cd1b26b9cedd8cc1029aaf6a1e3b9292ae7bc9c
alabliuk <andrelabliuk@gmail.com>
2022-06-22 01:46:27 -0300

    VowelCount

---
+++ dotnet/VowelCount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static void Main()
+    {
+        GetVowelCount("abracadabrae");
+    }
+
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+
+        vowelCount = Regex.Matches(str, @"[aeiou]").Count;
+
+        return vowelCount;
+    }
+}
