3770d61537e48254106b76a79746040dd94b2b21
Andre Leme <andre.leme@gertec.com.br>
2022-06-23 19:28:13 -0300

    rot13

---
+++ dotnet/Rot13/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+    //--> https://www.codewars.com/kata/530e15517bc88ac65600071
+
+    public static void Main()
+    {
+        Rot13("test"); //grfg
+    }
+
+    public static string Rot13(string message)
+    {
+        // your code here
+
+        return message;
+    }
+}
