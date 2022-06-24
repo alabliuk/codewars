420455ccb658322b6dbdbe57a9fd8ed0f8865f88
Andre Leme <andre.leme@gertec.com.br>
2022-06-24 13:21:36 -0300

    FindThePosition

---
+++ dotnet/FindThePosition/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Kata
+{
+	//--> https://www.codewars.com/kata/5808e2006b65bff35500008f
+
+	public static string PHRASE = "abcdefghijklmnopqrstuvwxyz";
+
+	public static void Main()
+	{
+		var result = Position(Convert.ToChar("a"));
+	}
+
+	public static string Position(char alphabet)
+	{
+		char[] positions = PHRASE.ToCharArray();
+
+		int i = 1;
+
+		foreach (char x in positions)
+		{
+			if (x == alphabet)
+			{
+				return ($"Position of alphabet: {i}");
+			}
+			i++;
+		}
+
+		return "Not Found!";
+	}
+}
