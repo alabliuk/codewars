983e099aaa451d30b59bf9a4a7f0b1dcfdaff769
Andre Leme <andre.leme@gertec.com.br>
2022-06-24 13:53:58 -0300

    ReplaceWithAplphabetPosition

---
+++ dotnet/ReplaceWithAlphabetPosition/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Kata
+{
+	//--> https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+	public static void Main()
+	{
+		var result = AlphabetPosition("The sunset sets at twelve o' clock.");
+	}
+
+	public static string AlphabetPosition(string text)
+	{
+		string alphabet = "0abcdefghijklmnopqrstuvwxyz";
+		var result = string.Empty;
+		foreach (char x in text.ToLower())
+		{
+			int position = alphabet.IndexOf(x);
+			if (position > 0)
+				result += string.Concat(position, " ");
+		}
+
+		return result.TrimEnd();
+	}
+}
