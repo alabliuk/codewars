4c77f0960440c7e777912047ef81f1cde674b9a7
alabliuk <andrelabliuk@gmail.com>
2022-06-26 17:35:35 -0300

    uniqueorder

---
+++ dotnet/UniqueInOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public static class Kata
+{
+    //--> https://www.codewars.com/kata/54e6533c92449cc251001667
+
+    public static void Main()
+    {
+        UniqueInOrder("AAAABBBCCDAABBB");
+    }
+
+    public static int UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+        var uniqueOrder = new string[] { };
+
+        foreach (var x in iterable)
+        {
+            uniqueOrder[1] = x.ToString();
+        }
+
+        return 1;
+    }
+}
