b720ca94499446b23cc3594f1ac9fd0bdce944a9
alabliuk <andrelabliuk@gmail.com>
2022-06-26 03:07:04 -0300

    LineNumbering

---
+++ dotnet/Testing 123/Program.cs
@@ -0,0 +1,19 @@
+﻿public class LineNumbering
+{
+    //--> https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9
+
+    public static void Main()
+    {
+        Number(new List<string> { "a", "b", "c" });
+    }
+
+    public static List<string> Number(List<string> lines)
+    {
+        for (int i = 0; i < lines.Count; i++)
+        {
+            lines[i] = $"{i + 1}: {lines[i]}";
+        }
+
+        return lines;
+    }
+}
