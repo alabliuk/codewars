64b642ad70e22f7b053ef0897374acda800475e7
alabliuk <andrelabliuk@gmail.com>
2022-06-27 22:35:53 -0300

    IsUpperCase

---
+++ dotnet/IsUpperCase/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class StringExtensions
+{
+    public static void Main()
+    {
+        IsUpperCase("hello I AM DONALD"); //false  
+    }
+
+    public static bool IsUpperCase(this string text)
+    {
+        char[] txtArray = text.ToArray();
+
+        foreach(var x in txtArray)
+        {
+            if (char.IsLower(x))
+                return false;
+        }
+
+        return true;
+    }
+}
