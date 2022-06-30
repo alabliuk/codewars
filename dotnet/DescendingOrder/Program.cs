60ef54bf4dd0ebf006a462eae79542b13320d2ab
alabliuk <andrelabliuk@gmail.com>
2022-06-30 20:09:33 -0300

    inacabados

---
+++ dotnet/DescendingOrder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    //--> https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+    public static void Main()
+    {
+        DescendingOrder(1021); //1201
+        //DescendingOrder(123);
+    }
+
+    public static int DescendingOrder(int num)
+    {
+        var invertArray = num.ToString().ToArray().Reverse();
+        string newNum = string.Empty;
+
+        foreach(var x in invertArray)
+        {
+            newNum += char.GetNumericValue(x);
+        }
+
+        return Convert.ToInt32(newNum);
+    }
+}
---
+++ dotnet/SortingPlanetTwisted37/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Kata
+{
+    //--> https://www.codewars.com/kata/58068479c27998b11900056e/
+
+    public static void Main()
+    {
+        //SortTwisted37(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        SortTwisted37(new[] { 9, 2, 4, 7, 3 }); //2,7,4,3,9
+    }
+
+    public static int[] SortTwisted37(int[] array)
+    {
+        var newArray = array.OrderBy(x => x)
+            .Where(x => x == 3)
+            .ToArray();
+
+
+
+
+        if (array.Length >= 7)
+        {
+            var aux = newArray[2];
+            newArray[2] = newArray[6];
+            newArray[6] = aux;
+        }
+        else
+        {
+            var aux = newArray[2];
+            newArray[2] = newArray[array.Length - 1];
+            newArray[array.Length - 1] = aux;
+        }
+
+        return newArray;
+    }
+}
