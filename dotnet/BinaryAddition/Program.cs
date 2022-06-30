a2ec73c092731e134da705ddbc5e86a99b1d3386
Andre Leme <andre.leme@gertec.com.br>
2022-06-30 16:08:28 -0300

    century

---
+++ dotnet/BinaryAddition/Program.cs
@@ -0,0 +1,14 @@
+﻿public static class Kata
+{
+	//--> https://www.codewars.com/kata/551f37452ff852b7bd000139
+
+	public static void Main()
+	{
+		var result = AddBinary(1, 2);
+	}
+
+	public static string AddBinary(int a, int b)
+	{
+		return Convert.ToString((a + b), 2);
+	}
+}
---
+++ dotnet/CenturyFromYear/Program.cs
@@ -0,0 +1,19 @@
+﻿public static class Kata
+{
+	public static void Main()
+	{
+		СenturyFromYear(1705);
+		СenturyFromYear(2000);
+		СenturyFromYear(2002);
+	}
+
+	public static int СenturyFromYear(int year)
+	{
+		var century = year / 100;
+
+		if (year != (century * 100))
+			century += 1;
+
+		return century;
+	}
+}
