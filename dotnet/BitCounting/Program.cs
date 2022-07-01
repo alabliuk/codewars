0bc8bd1179a8aa50c23a171673f05cff8fa82a5c
Andre Leme <andre.leme@gertec.com.br>
2022-07-01 17:08:25 -0300

    CountBits

---
+++ dotnet/BitCounting/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+    //--> https://www.codewars.com/kata/526571aae218b8ee490006f4
+
+    public static void Main()
+    {
+        var result = CountBits(4);
+	}
+
+    public static int CountBits(int n)
+    {
+        var binary = Convert.ToString(n, 2);
+        return binary.Where(x => (x == '1')).Count();
+    }
+}
