d2c67926e8252316d0be634931d974b2031e7a92
Andre Leme <andre.leme@gertec.com.br>
2022-07-01 17:36:24 -0300

    Narcissistic

---
+++ dotnet/Narcissistic/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Kata
+{
+	//--> https://www.codewars.com/kata/5287e858c6b5a9678200083c
+
+	public static void Main()
+	{
+		Narcissistic(1652);
+	}
+
+	public static bool Narcissistic(int value)
+	{
+		var charValue = value.ToString().ToArray();
+		var calc = 0.0;
+
+		foreach (char c in charValue)
+		{
+			calc += Math.Pow(char.GetNumericValue(c), charValue.Length);
+		}
+
+		if (calc != value)
+			return false;
+
+		return true;
+	}
+}
