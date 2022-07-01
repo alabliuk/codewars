4ee29cf04390d4d797d50f01e37c3d83a19382aa
Andre Leme <andre.leme@gertec.com.br>
2022-07-01 18:38:46 -0300

    isprime

---
+++ dotnet/IsPrime/Program.cs
@@ -0,0 +1,22 @@
+﻿public static class Kata
+{
+	//--> https://www.codewars.com/kata/5262119038c0985a5b00029f
+	public static void Main()
+	{
+		var result = IsPrime(3);
+	}
+
+	public static bool IsPrime(int n)
+	{
+		if (n <= 1)
+			return false;
+
+		for (int i = n-1; i >= 2; i--)
+		{
+			if (n % i == 0)
+				return false;
+		}
+
+		return true;
+	}
+}
