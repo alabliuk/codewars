6ffbd55b1afd98282e61432f4e6c543a3b24419e
alabliuk <andrelabliuk@gmail.com>
2022-07-03 15:24:58 -0300

    uncensor

---
+++ dotnet/_done/UncensorStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Kata
+{
+    public static void Main()
+    {
+        Uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae"); //"This is very strange",
+    }
+
+    public static string Uncensor(string infected, string discovered)
+    {
+        var newStr = string.Empty;
+
+        for (int i = 0, j=0; i < infected.Count(); i++)
+        {
+            if (infected[i] == '*')
+            {
+                newStr += discovered[j];
+                j++;
+            }
+            else
+                newStr += infected[i];
+        }
+        return newStr;
+    }
+}
