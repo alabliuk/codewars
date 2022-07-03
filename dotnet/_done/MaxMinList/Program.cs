3ff64b6e3c2f1ae3e0f8d8c614aa3627849b571f
alabliuk <andrelabliuk@gmail.com>
2022-07-03 17:16:54 -0300

    MaxMin

---
+++ dotnet/_done/MaxMinList/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Kata
+{
+    //--> https://www.codewars.com/kata/577a98a6ae28071780000989
+    public static void Main()
+    {
+        Min(new[] { -52, 56, 30, 29, -54, 0, -110 }); //-110
+        Min(new[] { 42, 54, 65, 87, 0 }); //0
+        Max(new[] { 4, 6, 2, 1, 9, 63, -134, 566 }); //566
+        Max(new[] { 5 }); //5
+    }
+
+    public static int Min(int[] list)
+    {
+        return list.OrderBy(x => x).ToList().First();
+    }
+
+    public static int Max(int[] list)
+    {
+        return list.OrderByDescending(x => x).ToList().First();
+    }
+}
