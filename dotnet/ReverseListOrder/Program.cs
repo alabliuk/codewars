69836b3bade4737c1d4ba482a2cc89a6fca0cf17
Andre Leme <andre.leme@gertec.com.br>
2022-07-07 16:19:55 -0400

    ToCamelCase

---
+++ dotnet/ReverseListOrder/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Kata
+{
+	//--> https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+
+	public static void Main()
+	{
+		var result = ReverseList(new List<int> { 4, 3, 2, 1 });
+	}
+
+	public static List<int> ReverseList(List<int> list)
+	{
+		return Enumerable.Reverse(list).ToList();
+	}
+}
---
+++ dotnet/ToCamelCase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Kata
+{
+	//--> https://www.codewars.com/kata/517abf86da9663f1d2000003
+	public static void Main()
+	{
+		ToCamelCase("the_stealth_warrior");
+		ToCamelCase("The-Stealth-Warrior");
+	}
+
+	public static string ToCamelCase(string str)
+	{
+		var newStr = str.Split(new[] { '_', '-' });
+		var result = string.Empty;
+
+		for (int i = 0; i < newStr.Length; i++)
+		{
+			if (i > 0)
+				result += char.ToUpper(newStr[i][0]) + newStr[i].Substring(1);
+			else
+				result += newStr[i];
+		}
+
+		return result;
+	}
+}
