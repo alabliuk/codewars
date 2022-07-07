a73be5e97f84bc2e942e877c0ad5977e7164ed2b
Andre Leme <andre.leme@gertec.com.br>
2022-07-07 18:04:12 -0400

    countMinutes

---
+++ dotnet/MinutesToMidnight/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Minute
+{
+	//--> https://www.codewars.com/kata/58528e9e22555d8d33000163
+	public static void Main()
+	{
+		countMinutes(DateTime.Now.Date.AddHours(12)); //720 minutes
+		countMinutes(DateTime.Now.Date.AddHours(23).AddMinutes(59)); //1 minute
+	}
+
+	public static string countMinutes(DateTime d)
+	{
+
+		return "a";
+	}
+}
