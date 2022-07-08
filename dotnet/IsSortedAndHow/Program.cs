a326c687c34e754d5686df0bdcaf6a4bb1f6aa9a
Andre Leme <andre.leme@gertec.com.br>
2022-07-08 15:45:21 -0400

    IsSortedAndHow

---
+++ dotnet/IsSortedAndHow/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Kata
+{
+	//--> https://www.codewars.com/kata/580a4734d6df748060000045
+	public static void Main()
+	{
+		IsSortedAndHow(new[] { 1, 2 }); //"yes, ascending"
+		IsSortedAndHow(new[] { 15, 7, 3, -8 }); //"yes, descending"
+		IsSortedAndHow(new[] { 4, 2, 30 }); //"no"
+	}
+
+	public static string IsSortedAndHow(int[] array)
+	{
+		if (array[0] < array[1])
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				if (array[i - 1] > array[i])
+					return "no";
+			}
+			return "yes, ascending";
+		}
+		else if (array[0] > array[1])
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				if (array[i - 1] < array[i])
+					return "no";
+			}
+			return "yes, descending";
+		}
+		else
+			return "no";
+	}
+}
