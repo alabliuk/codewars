49a0fc81c4a84f120ce1f8eb11943feecd210bec
Andre Leme <andre.leme@gertec.com.br>
2022-07-11 14:22:43 -0400

    countDays

---
+++ dotnet/CollectionAssertTest/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NuitKata
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class Kata
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int PhoneNumber { get; set; }
+	}
+
+	public class AssertTest()
+	{
+	}
+}
---
+++ dotnet/CountDays/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Day
+{
+	//--> https://www.codewars.com/kata/5837fd7d44ff282acd000157
+	public static void Main()
+	{
+		var result = string.Empty;
+		result = countDays(new DateTime(2016, 12, 2)); //"The day is in the past!"
+		result = countDays(DateTime.Now); //"Today is the day!"\
+		result = countDays(new DateTime(2022, 07, 15)); //
+	}
+
+	public static string countDays(DateTime d)
+	{
+		if (d.Date == DateTime.Now.Date)
+			return "Today is the day!";
+		else
+		{
+			var count = d - DateTime.Now;
+			if (count.Days < 0)
+				return "The day is in the past!";
+			else
+			{
+				return $"{count.Days} days";
+			}
+		}
+	}
+}
