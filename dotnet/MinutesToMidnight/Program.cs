ecf3cbcb37bd22ccff36340f94853400f978ca15
Andre Leme <andre.leme@gertec.com.br>
2022-07-12 09:58:55 -0400

    fix folders

--- dotnet/MinutesToMidnight/Program.cs
+++ dotnet/MinutesToMidnight/Program.cs
@@ -5,13 +5,14 @@ public class Minute
 	//--> https://www.codewars.com/kata/58528e9e22555d8d33000163
 	public static void Main()
 	{
-		countMinutes(DateTime.Now.Date.AddHours(12)); //720 minutes
-		countMinutes(DateTime.Now.Date.AddHours(23).AddMinutes(59)); //1 minute
+		var result = string.Empty;
+		result = countMinutes(DateTime.Now.Date.AddHours(12)); //720 minutes
+		result = countMinutes(DateTime.Now.Date.AddHours(23).AddMinutes(59)); //1 minute
 	}
 
 	public static string countMinutes(DateTime d)
 	{
-
-		return "a";
+		var calcMinutes = DateTime.Now - d;
+		return (calcMinutes.Minutes > 1) ? $"{calcMinutes.Minutes} minutes" : $"{calcMinutes.Minutes} minutes";
 	}
 }
